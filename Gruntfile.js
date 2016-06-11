df92a4e9af000cd3c585e0bbec9cd8d2d420be02
cat_in_136 <cat.in.136+github@gmail.com>
2016-06-11 19:00:53 +0900

    initial commit, just scaffold.

---
+++ Gruntfile.js
@@ -0,0 +1,90 @@
+module.exports = function(grunt) {
+  var path = require("path");
+
+  grunt.initConfig({
+    copy: {
+      bower_components: {
+        files: [
+          {
+            expand: true,
+            cwd: "bower_components/bootstrap/dist",
+            src: ["**"],
+            dest: "public/lib/bootstrap"
+          },
+          {
+            expand: true,
+            cwd: "bower_components/jquery/dist",
+            src: ["**"],
+            dest: "public/lib/jquery"
+          }
+        ]
+      },
+      main: {
+        files: [
+          {
+            expand: true,
+            cwd: "src/html",
+            src: ["*.html"],
+            dest: "public"
+          },
+        ],
+      },
+      js: {
+        files: [
+          {
+            expand: true,
+            cwd: "src/js",
+            src: ["*.js", "*.es"],
+            dest: "public/js",
+          }
+        ]
+      }
+    },
+    babel: {
+      dist: {
+        options: {
+          sourceMap: true,
+          presets: "es2015"
+        },
+        files: {
+          "public/js/main.js": "public/js/main.es"
+        }
+      }
+    },
+    watch: {
+      copy_main: {
+        files: "src/html/*",
+        tasks: ["copy:main"],
+        options: {
+          livereload: true
+        }
+      },
+      babel: {
+        files: "src/js/*",
+        tasks: ["copy:js", "babel"],
+        options: {
+          livereload: true
+        }
+      }
+    },
+    connect: {
+      server: {
+        options: {
+          port: 9000,
+          livereload: true,
+          base: "public"
+        }
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-babel');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-connect');
+
+  grunt.registerTask("init", ["clean", "bower:install"]);
+  grunt.registerTask("build", ["copy:bower_components", "copy:main", "copy:js", "babel"]);
+  grunt.registerTask("serve", ["connect", "watch"]);
+  grunt.registerTask("default", ["serve"]);
+};
